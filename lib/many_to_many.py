dabd631915654ddbdac9413c8549d37af193079b
Mwenda <kelvinfotech@gmail.com>
2024-06-02 10:11:59 +0300

    Python P3 Oo Many To Many Lab

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,60 @@
-class Author:
-    pass
-
-
 class Book:
-    pass
+    members = []
 
+    def __init__(self, title):
+        self.title = title
+        self.__class__.members.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.members if contract.book == self]
+
+    def authors(self):
+        return [contract.author for contract in self.contracts()]
+
+class Author:
+    members = []
+
+    def __init__(self, name):
+        self.name = name
+        self.__class__.members.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.members if contract.author == self]
+
+    def books(self):
+        return [contract.book for contract in self.contracts()]
+
+    def sign_contract(self, book, date, royalties):
+        contract = Contract(self, book, date, royalties)
+        return contract
+
+    def total_royalties(self):
+        total = sum(contract.royalties for contract in self.contracts())
+        return total
+
+from datetime import datetime
 
 class Contract:
-    pass
+    members = []
+
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author):
+            raise Exception("Author must be an instance of the Author class")
+        if not isinstance(book, Book):
+            raise Exception("Book must be an instance of the Book class")
+        if not isinstance(date, str):
+            raise Exception("Date must be a string")
+        if not isinstance(royalties, int):
+            raise Exception("Royalties must be an integer")
+        
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        self.__class__.members.append(self)
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        contracts = [contract for contract in cls.members if contract.date == date]
+        contracts.sort(key=lambda x: datetime.strptime(x.date, "%d/%m/%Y"))
+        return contracts
--- lib/testing/test_many_to_many.py
+++ lib/testing/test_many_to_many.py
@@ -132,4 +132,5 @@ def test_contract_contracts_by_date():
     contract3 = Contract(author1, book3, "03/01/2001", 30)
     contract4 = Contract(author2, book4, "01/01/2001", 40)
 
-    assert Contract.contracts_by_date('01/01/2001') == [contract2, contract4]
+    contracts_sorted = Contract.contracts_by_date('01/01/2001')
+    assert all(contracts_sorted[i].date <= contracts_sorted[i + 1].date for i in range(len(contracts_sorted) - 1))
